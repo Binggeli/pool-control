b17ca6d7e5f48fd5bd978e65055156fe2d59adfe
Beat Scherer <beat.github@sunnig.ch>
2020-05-18 06:19:55 +0000

    Add class for pool data

---
+++ pool_status.py
@@ -0,0 +1,87 @@
+from pprint import pprint
+from datetime import datetime
+from pathlib import Path
+import os
+import json
+
+import pool_data as pd
+
+DATE_FORMAT = r'%Y-%m-%d %H:%M:%S'
+DATA_PATH = Path.home() / "Documents"
+LATESTDATA_PATH = DATA_PATH / "latestdata.json"
+
+
+class PoolStatus:
+    """Contains status information of the pool.
+
+    This class implements methods to save objects to json files and load them
+    again. As some attributes are datetime objects, which are loaded as strings
+    from json, it needs to know which attributes have to be converted.
+    """
+
+    def __init__(self, timestamp=datetime.now()):
+        self.timestamp = timestamp
+        self.temperature = {}
+        self.min_temperature = {}
+        self.max_temperature = {}
+        self.light = 0
+        self.pump = {}
+
+    def __str__(self):
+        return 'PoolStatus as of {0}'.format(self.timestamp.strftime(DATE_FORMAT))
+
+    def __repr__(self):
+        return 'PoolStatus({0}): {1}'.format(repr(self.timestamp),
+                                             repr(self.__dict__()))
+
+    def update(self):
+        "Update status information."
+        self.temperature['water'] = pd.temperature(pd.POOL_TEMP_SENSOR)
+        self.temperature['surface'] = pd.temperature(pd.SURFACE_TEMP_SENSOR)
+        self.temperature['air'] = pd.temperature(pd.AIR_TEMP_SENSOR)
+        self.light = pd.light_intensity()
+        self.pump['status'] = pd.pump_status()
+        self.pump['pressure'] = pd.pressure(pd.FILTER_PRESSURE_CHANNEL)
+        self.pump['power'] = pd.powerconsumption(pd.POWER_CONSUMPTION_CHANNEL)
+        self.timestamp = datetime.now()
+        return self
+
+    def json(self):
+        "Return object as json-encoded string."
+        data = self.__dict__.copy()
+        data['timestamp'] = self.timestamp.strftime(DATE_FORMAT)
+        return json.dumps(data)
+
+    def save(self, latest=True):
+        """Save object to a json file and return its path.
+
+        The filename contains the timestamp of the data object.
+        If latest is True (default) a symbolic link is created
+        with the name "latestdata".
+        """
+        datafile = DATAPATH / "data_{:%Y%m%d%H%M%S}.json".format(timestamp)
+        datafile.write_text(self.json())
+        if latest:
+            os.symlink(str(datafile),
+                       str(LATESTDATAPATH))
+        return str(datafile)
+
+    @classmethod
+    def load(cls):
+        "Return the latest data object loaded from the json file."
+        try:
+            data = json.load(LATESTDATAPATH.read_text())
+        except FileNotFoundError:
+            return cls().update()
+        else:
+            obj = cls()
+            obj.temperature = data['temperature']
+            obj.light = data['light']
+            obj.pump = data['pump']
+            obj.timestamp = datetime.strptime(data['timestamp'],
+                                              DATE_FORMAT)
+            return obj
+
+
+if __name__ == "__main__":
+    pprint(PoolData().update())
