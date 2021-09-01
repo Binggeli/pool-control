71068aab626b9c702a1f96189c7ae38f5fe2b524
Beat Scherer <beat.github@sunnig.ch>
2021-09-01 12:04:27 +0000

    Add more sensors to MQTT

--- pool_send.py
+++ pool_send.py
@@ -12,6 +12,10 @@ while True:
         client.publish("sensors/pool/temperature/water", pd.temperature(pd.POOL_TEMP_SENSOR), qos=2)
         client.publish("sensors/pool/temperature/surface", pd.temperature(pd.SURFACE_TEMP_SENSOR), qos=2)
         client.publish("sensors/pool/temperature/air", pd.temperature(pd.AIR_TEMP_SENSOR), qos=2)
+        client.publish("sensors/pool/temperature/cpu", pd.cpu_temperature(), qos=2)
+        client.publish("sensors/pool/temperature/gpu", pd.gpu_temperature(), qos=2)
+        client.publish("sensors/pool/light", pd.light_intensity(), qos=2)
+        client.publish("sensors/pool/pump", pd.pump_status(), qos=2)
     except KeyboardInterrupt:
         client.loop_stop()
         exit(0)
