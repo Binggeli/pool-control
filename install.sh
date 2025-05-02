df378e0ee3915ff4754a5bb452988faeb5ef3fd0
Beat Scherer <beat.scherer@sunnig.ch>
2025-05-02 09:21:41 +0200

    Implement install script

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# update python sources
+rsync -rtv --del bin/ ~/bin/
+
+# update and activate services
+cd svc
 for svc in *.service
 do
   cp "${svc}" /lib/systemd/system
@@ -7,4 +12,3 @@ do
   systemctl start "${svc}"
   systemctl enable "${svc}"
 done
-
